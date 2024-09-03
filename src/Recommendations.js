8a62bb22edabebc9b1d0fa0080aa2711fb0a2f43
MICHELLCGs <128429273+MICHELLCGs@users.noreply.github.com>
2024-09-03 08:02:21 -0500

    Recomendations changes

--- src/Recommendations.js
+++ src/Recommendations.js
@@ -1,36 +1,52 @@
 import React, { useEffect, useState } from 'react';
+import './Recommendations.css';
 
 function Recommendations() {
   const [recommendations, setRecommendations] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Hacer la solicitud a la API utilizando la variable de entorno
     fetch(process.env.REACT_APP_API_URL)
       .then(response => response.json())
       .then(data => {
-        setRecommendations(data.recommendations);
+        console.log('Fetched data:', data); // Imprimir los datos para ver la estructura
+        if (Array.isArray(data.persons)) {
+          setRecommendations(data.persons);
+        } else {
+          console.error('Expected an array for persons, but got:', data.persons);
+          setError('Unexpected data format');
+        }
       })
       .catch(error => {
         console.error('Error fetching data:', error);
+        setError('Failed to fetch data');
       });
   }, []);
 
+  if (error) {
+    return <div className="error">Error: {error}</div>;
+  }
+
   return (
-    <div>
-      <h1>Esta es mi Recommendations </h1>
+    <div className="container">
+      <h1 className="header">Recomendaciones</h1>
       <ul>
-        {recommendations.map(person => (
-          <li key={person.person_ID}>
-            <h2>{person.name}</h2>
-            <p><strong>First Name:</strong> {person.first}</p>
-            <p><strong>Last Name:</strong> {person.last}</p>
-            <p><strong>Middle Name:</strong> {person.middle || 'N/A'}</p>
-            <p><strong>Email:</strong> {person.email}</p>
-            <p><strong>Phone:</strong> {person.phone}</p>
-            <p><strong>Fax:</strong> {person.fax}</p>
-            <p><strong>Title:</strong> {person.title}</p>
-          </li>
-        ))}
+        {recommendations.length > 0 ? (
+          recommendations.map(person => (
+            <li key={person.personID} className="card">
+              <h2>{person.name}</h2>
+              <p><strong>First Name:</strong> {person.first}</p>
+              <p><strong>Last Name:</strong> {person.last}</p>
+              <p><strong>Middle Name:</strong> {person.middle || 'N/A'}</p>
+              <p><strong>Email:</strong> {person.email}</p>
+              <p><strong>Phone:</strong> {person.phone}</p>
+              <p><strong>Fax:</strong> {person.fax}</p>
+              <p><strong>Title:</strong> {person.title}</p>
+            </li>
+          ))
+        ) : (
+          <p>No recommendations available</p>
+        )}
       </ul>
     </div>
   );
